4c90decbe91fdd0d5ab87f64fd39b78536b049c6
mhAbdullah22 <abdullah37822@gmail.com>
2025-04-22 12:21:31 +0600

    active class added

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,6 +3,7 @@ import logo from '../../assets/C001-assets/logo.png'
 import { FaFacebook } from "react-icons/fa";
 import { FaLinkedin } from "react-icons/fa";
 import { FaGithub } from "react-icons/fa";
+import {  NavLink } from 'react-router';
 const footer = () => {
   return (
        <div className="navbar flex flex-col items-center justify-center gap-3 py-8 ">
@@ -15,12 +16,12 @@ const footer = () => {
     </div>
     <div className="navbar-center border-b-2 border-gray-400 px-16">
       <ul className="menu menu-horizontal px-1">
-      <li><a>Home</a></li>
-          <li>
-           <a>My-Bookings</a>
-          </li>
-          <li><a>Blogs</a></li>
-          <li><a>Contact Us</a></li>
+      <li><NavLink>Home</NavLink></li>
+        <li>
+        <NavLink>My Bookings</NavLink>
+        </li>
+        <li><NavLink>Blogs</NavLink></li>
+        <li><NavLink>Contact Us</NavLink></li>
       </ul>
     </div>
     <div className="navbar-end flex gap-4 mr-[500px] pt-2">
--- src/components/Header/Navbar.jsx
+++ src/components/Header/Navbar.jsx
@@ -1,23 +1,31 @@
 import React from 'react';
 import logo from '../../assets/C001-assets/logo.png'
+import {  NavLink } from 'react-router';
+import '../Header/header.css'
 const Navbar = () => {
+  const links=<>
+        <li><NavLink to='/'>Home</NavLink></li>
+        <li>
+        <NavLink>My Bookings</NavLink>
+        </li>
+        <li><NavLink>Blogs</NavLink></li>
+        <li><NavLink>Contact Us</NavLink></li>
+ </>
+
   return (
-    
-      <div className="navbar ">
+      
+      <div className='navbar' >
   <div className="navbar-start">
     <div className="dropdown">
       <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /> </svg>
       </div>
-      <ul
+      <ul 
         tabIndex={0}
         className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
-        <li><a>Home</a></li>
-        <li>
-         <a>My-Bookings</a>
-        </li>
-        <li><a>Blogs</a></li>
-        <li><a>Contact Us</a></li>
+        {
+          links
+        }
       </ul>
     </div>
    <div className='flex flex-row items-center gap-3'>
@@ -27,12 +35,9 @@ const Navbar = () => {
   </div>
   <div className="navbar-center hidden lg:flex">
     <ul className="menu menu-horizontal px-1">
-    <li><a>Home</a></li>
-        <li>
-         <a>My-Bookings</a>
-        </li>
-        <li><a>Blogs</a></li>
-        <li><a>Contact Us</a></li>
+    {
+      links
+    }
     </ul>
   </div>
   <div className="navbar-end">
